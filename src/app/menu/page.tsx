feb703c416c9bcec10a3e62023236e9f44ede187
adelmodias <contato@adelmodias.com.br>
2023-09-21 00:00:33 -0300

    restaurant food ordering app

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { menu } from "../data";
 
+// Menu Page
 const MenuPage = () => {
     return (
         <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
